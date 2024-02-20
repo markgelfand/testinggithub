ac9309d7fd233f0dde7a602314a22e62c0bf00d0
maya <126mgupta@frhsd.com>
2024-02-20 12:22:38 -0500

    For Loop + I am 16 years old

--- first github/src/Mark_Gelfand.java
+++ first github/src/Mark_Gelfand.java
@@ -4,6 +4,10 @@ public class Mark_Gelfand {
 
         System.out.println("Hello Mark");
         System.out.println("Hello Tyler");
+
+        for(int i = 0; i < 16; i++) {
+            System.out.println("I'm 16 years old");
+        }
     }
 }
 
