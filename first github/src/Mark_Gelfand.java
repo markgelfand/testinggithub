4ef29bbf4f4402c83068d263ae436b5536e7ec99
markgelfand <113187474+markgelfand@users.noreply.github.com>
2024-02-20 12:51:26 -0500

    Revert "Mark Pull Request"

--- first github/src/Mark_Gelfand.java
+++ first github/src/Mark_Gelfand.java
@@ -1,13 +1,3 @@
 public class Mark_Gelfand {
-
-    public static void main(String[] args){
-
-        System.out.println("Hello Mark");
-        System.out.println("Hello Tyler");
-
-        for(int i = 0; i < 16; i++) {
-            System.out.println("I'm 16 years old");
-        }
-    }
+    
 }
-
